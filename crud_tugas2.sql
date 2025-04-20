c67fb72285786ec880b4262542113f2bd66e543f
Irish Jovincia <irishjovinciaa@gmail.com>
2025-04-20 11:43:35 +0700

    update

--- crud_tugas2.sql
+++ crud_tugas2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 20, 2025 at 06:29 AM
+-- Generation Time: Apr 20, 2025 at 06:39 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `crud_notes`
+-- Database: `crud_tugas2`
 --
 
 -- --------------------------------------------------------
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `gender` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -40,8 +40,11 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `gender`, `createdAt`, `updatedAt`) VALUES
-(1, 'cencen EDIT', 'cencen@gmail.com', 'female', '2025-03-17 19:18:28', '2025-03-17 19:19:36');
+INSERT INTO `users` (`id`, `date`, `title`, `content`, `createdAt`, `updatedAt`) VALUES
+(2, '2025-03-02 17:01:44', 'Lexie Cencen', 'lexie itu anjingku, cencen itu kucingku', '2025-03-02 17:01:44', '2025-03-04 00:31:46'),
+(3, '2025-03-02 17:04:27', 'Aku Sedih', 'aku sedih banget karena banyak tugas huhuhu', '2025-03-02 17:04:27', '2025-03-02 17:28:41'),
+(4, '2025-03-03 00:00:00', 'Notulensi Rapat', 'rapat jam 5 di Joglo Klaten membahas acara 17an', '2025-03-02 18:57:21', '2025-03-02 18:57:21'),
+(13, '2025-03-24 00:00:00', 'Halo', 'coba coba', '2025-03-24 22:42:57', '2025-03-24 22:42:57');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
