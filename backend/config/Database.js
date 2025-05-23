f0a7f1d4293eb5cd94fdaa07e16a862634475b81
Irish Jovincia <irishjovinciaa@gmail.com>
2025-05-23 13:31:32 +0700

    update

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -7,10 +7,8 @@ const db = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
+    host: process.env.DB_HOST,  // IP publik MySQL
     dialect: "mysql",
-    dialectOptions: {
-      socketPath: process.env.DB_HOST // Contoh: /cloudsql/project:region:instance-name
-    },
     logging: false,
   }
 );
