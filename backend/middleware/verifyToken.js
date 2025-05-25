ea93e6b3d0a8af73d7fa94bf95510ca615213abc
Irish Jovincia <irishjovinciaa@gmail.com>
2025-05-26 03:28:15 +0700

    update

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const verifyToken = (req, res, next) => {
+  console.log("Authorization header:", req.headers.authorization);
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ error: "Akses ditolak. Token tidak tersedia." });
-
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
